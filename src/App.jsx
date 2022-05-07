bb336136abefb212f472ec422875fa99c8d4ad60
febri4n <solihinfebrian0@gmail.com>
2022-05-07 15:24:59 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Analytics from './components/Analytics'
+import Cards from './components/Cards';
+import Footer from './components/Footer';
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Newsletter from './components/Newsletter'
+
+export default function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
+    </div>
+  )
+}
